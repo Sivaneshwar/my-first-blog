c1762473d5f0e8da76a708b47109b940c41cab75
Sivaneshwar <sivaneshwar.p@yahoo.com>
2018-10-16 10:54:34 +0530

    Skynet Blog v.1.0 Stable

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7om0g-0^41^g0h+$zostfiuuf9^*m%kok3@l$eig0kibom#ebn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
