334e2f98bad08900c7828f2c8141c4ee983eb98b
Sivaneshwar <sivaneshwar.p@yahoo.com>
2019-07-30 10:04:53 +0530

    Home switch

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('home/',views.post_list, name = 'post_list'),
-	path('',views.show_file, name = 'show_file'),
-	path('showJson/',views.show_json, name = 'show_json'),
+	path('showFile/',views.show_file, name = 'show_file'),
+	path('',views.show_json, name = 'show_json'),
 	path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 	path('post/new',  views.post_new, name = 'post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
